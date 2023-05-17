c4b05a881584a1ec6019695af45b36b4e035a744
AngkitHashamsa <AngkitHashamsa21@gmail.com>
2023-05-17 16:13:15 +0530

    return -1 when target not present

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ function linearSearch(arr, t) {
 function binarySearch(arr, target) {
     let leftIndex = 0;
     let rightIndex = arr.length - 1;
+
     while (leftIndex <= rightIndex) {
         let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
 
@@ -116,4 +117,8 @@ function binarySearch(arr, target) {
             leftIndex = middleIndex + 1;
         }
     }
+
+    return -1;
 }
+
+console.log(binarySearch([1, 2, 3, 4, 5], 5));
